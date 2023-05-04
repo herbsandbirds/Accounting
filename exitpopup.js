21b078b38eda49e4068b98364e7bed15103e8e21
herbsandbirds <rizzyullah@gmail.com>
2023-05-04 21:22:20 +0100

    Updated for SEO

--- exitpopup.js
+++ exitpopup.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   // Show the Contact Us form after 2 minutes
   setTimeout(function() {
     document.getElementById("contact-us-popup").style.display = "block";
-  }, 20*60*1000); // Change this value to the desired time interval (e.g., 2 * 60 * 1000 for 2 minutes)
+  }, 2*60*1000); // Change this value to the desired time interval (e.g., 2 * 60 * 1000 for 2 minutes)
 
   // Close the Contact Us form when the Close button is clicked
   document.getElementById("close-popup").addEventListener("click", function() {
